8df8c651f75de80bb87c139ba486739616609ccc
Rizaldi Aziz <rizaldiaziz18@gmail.com>
2023-02-20 23:16:43 +0700

    player movement

--- Player/Player.cs
+++ Player/Player.cs
@@ -3,19 +3,17 @@ using System;
 
 public class Player : KinematicBody2D
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
+	Vector2 velocity = Vector2.Zero;
+	public override void _PhysicsProcess(float delta){
+		Vector2 inputVector = Vector2.Zero;
+		inputVector.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
+		inputVector.y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		GD.Print("Hello World");	
+		if (inputVector != Vector2.Zero){
+			velocity = inputVector;
+		} else {
+			velocity = Vector2.Zero;
+		}
+		MoveAndCollide(velocity);
 	}
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
 }
