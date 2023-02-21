7f96628073285c1b62009fedf69d520ec3b19daa
Rizaldi Aziz <rizaldiaziz18@gmail.com>
2023-02-21 11:27:56 +0700

    add collision and bush scene

--- Player/Player.cs
+++ Player/Player.cs
@@ -3,17 +3,23 @@ using System;
 
 public class Player : KinematicBody2D
 {
+	const int ACCELERATION = 500;
+	const int MAX_SPEED = 80;
+	const int FRICTION = 500;
 	Vector2 velocity = Vector2.Zero;
 	public override void _PhysicsProcess(float delta){
 		Vector2 inputVector = Vector2.Zero;
 		inputVector.x = Input.GetActionStrength("ui_right") - Input.GetActionStrength("ui_left");
 		inputVector.y = Input.GetActionStrength("ui_down") - Input.GetActionStrength("ui_up");
-
+		inputVector = inputVector.Normalized();
+		GD.Print(inputVector);
 		if (inputVector != Vector2.Zero){
-			velocity = inputVector;
+			velocity = velocity.MoveToward(inputVector * MAX_SPEED, ACCELERATION * delta);
+			// velocity += inputVector  * ACCELERATION * delta;
+			// velocity = velocity.LimitLength(MAX_SPEED * delta);
 		} else {
-			velocity = Vector2.Zero;
+			velocity = velocity.MoveToward(Vector2.Zero, FRICTION * delta);
 		}
-		MoveAndCollide(velocity);
+		velocity = MoveAndSlide(velocity);
 	}
 }
