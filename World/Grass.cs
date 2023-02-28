1e7d6882e86a5208b32591d206dfba190e6a4333
Rizaldi Aziz <rizaldiaziz18@gmail.com>
2023-02-28 23:42:42 +0700

    grass destroyed animation

--- World/Grass.cs
+++ World/Grass.cs
@@ -7,6 +7,11 @@ public class Grass : Node2D
     public override void _Process(float delta)
     {
         if (Input.IsActionJustPressed("attack")){
+            PackedScene GrassEffect = GD.Load<PackedScene>("res://Effects/GrassEffect.tscn");
+            Node2D grassEffect = GrassEffect.Instance<Node2D>();
+            Node world = GetTree().CurrentScene;
+            world.AddChild(grassEffect);
+            grassEffect.GlobalPosition = GlobalPosition;
             QueueFree();
         }
     }
